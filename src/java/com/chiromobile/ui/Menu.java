c9706ffcae9f99ae4063695dbae3dbe657761a6d
Randy Wandell <randy@bronlinesolutions.com>
2013-08-23 11:26:16 -0400

    Test changes in NetBeans

--- src/java/com/chiromobile/ui/Menu.java
+++ src/java/com/chiromobile/ui/Menu.java
@@ -26,6 +26,7 @@ public class Menu extends GenericServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         out.print("hello");
     } 
